9aaaab9ebdc058765eecc264e8c4a7e0c676967e
Maksatai <erkbvna@gmail.com>
2021-07-14 12:43:31 +0600

    add report

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask,render_template,redirect
+from flask import Flask,render_template
+from openpyxl import load_workbook
+
+excel=load_workbook('report.xlsx')
+page=excel["Sheet"]
 
 app=Flask(__name__)
 
 @app.route('/')
 def homepage():
-    f=open('goods.txt','r',encoding='utf-8')
-    txt=f.readlines()
-    return render_template('index.html',goods=txt)
+    return render_template('index.html',goods=page.values)
